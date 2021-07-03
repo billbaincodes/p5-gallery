a1fbe5e06d63641747ed309c39258deb951503cd
bill <williamandrewbain@gmail.com>
2021-07-03 10:37:54 -0600

    Add eyeball movement

--- gallery/eye-see-you/sketch.js
+++ gallery/eye-see-you/sketch.js
@@ -1,32 +1,57 @@
 let width = 400
 let height = 400
 
+let eyeballList = [];
+
 function setup() {
   createCanvas(height, width);
+  for (let i = 0; i <= width; i+=40) {
+    for (let ii = 0; ii <= height; ii+=40) {
+      let eyeball = new Eye(i, ii);
+      eyeballList.push(eyeball)
+    }
+  }
+  console.log({ eyeballList })
 }
 
 function draw() {
   background(220);
-
-
-
-
-  for (let i = 0; i <= width; i+=40) {
-    for (let ii = 0; ii <= height; ii+=40) {
-        fill('white')
-        ellipse(i, ii, 30, 24);
-        randomFill();
-        let xOff = random(-4, 4)
-        let yOff = random(-4, 4)
-        circle(i + xOff, ii + yOff, 10)
-    }
-  }
-  noLoop();
+  eyeballList.forEach(eyeball => {
+    eyeball.draw();
+    eyeball.wiggle();
+  })
 }
 
 function randomFill(){
     r = random(100);
     g = random(255);
     b = random(255);
-    return fill(r,g,b,200);
+    return { r, g, b, a: 200}
+}
+
+class Eye {
+  constructor(x, y){
+    this.x = x;
+    this.y = y;
+    this.centerX = x;
+    this.centerY = y;
+    this.color = randomFill();
+  }
+
+  wiggle(){
+    console.log('fuuuu')
+    this.x = this.x + random(-4, 4);
+    if (this.x > this.centerX + 4){ this.x = this.centerX + 4 }
+    if (this.x < this.centerX - 4){ this.x = this.centerX - 4 }
+    this.y = this.y + random(-4, 4);
+    if (this.y > this.centerY + 4){ this.y = this.centerY + 4 }
+    if (this.y < this.centerY - 4){ this.y = this.centerY - 4 }
+  }
+
+  draw(){
+    fill('white')
+    ellipse(this.x, this.y, 30, 24);
+    fill(this.color.r, this.color.g, this.color.b);
+    circle(this.x , this.y, 10);
+  }
 }
